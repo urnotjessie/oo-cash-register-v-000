6f6becaa2c582f96779d621b2ff61d83108b101f
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 20:57:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,7 @@ class CashRegister
 
     def add_item(title, price, quantity = 1)
         self.items << [title] * quantity
+        self.items.flatten
         self.total += price * quantity
     end
 

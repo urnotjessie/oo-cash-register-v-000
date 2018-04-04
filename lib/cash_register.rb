e8962a770ead3bd21e9acb509e62b4562f2551c9
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 21:11:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-    attr_accessor :total, :discount, :items
+    attr_accessor :total, :discount, :items, :last_transaction
 
     def initialize(discount = 0)
         @total = 0
